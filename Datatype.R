55d08afbb1754263d512daca4f9cec1a7e9d2465
wonhecho <50203463+wonhecho@users.noreply.github.com>
2021-07-27 11:20:12 +0900

    Update and rename Datatype to Datatype.R

---
+++ Datatype.R
@@ -0,0 +1,23 @@
+# as.integer() 정수형 변환
+#   .numeric() 숫자형 변환
+#   .ordered() 순서형 변환
+#   .Date()    날짜형으로 변환
+# is.integer() 정수형인지 확인
+#   .numeric() 숫자형인지 확인
+#   .logical() 논리형인지 확인
+#   .na()      NA형인지 확인
+#   .null()    NULL형인지 확인
+# Class()      데이터 형태 확인
+# SUM()        합
+# factor()     범주형 함수 만들기
+# nlevels()    범주형 변수 레벨 개수 반환
+# levels()     범주형 변수 레벨명 반환
+# ordered()    순셔형 변수 만들기
+# Sys.Date()   컴퓨터 운영상의 날짜
+# Sys.time()   날짜와 시간
+# date()       미국식 날짜와 시간 반환
+# seq()        일련의 숫자열, 날짜열 반환
+# objects()    저장된 변수들을 반환
+# ls()         "
+# rm()         변수 삭제
+# complete.cases() na가 아니면 T 반환
