6c64229082c0dfa1c8400803723cc4e0ad83ae7f
WonHeeCho <50203463+wonhecho@users.noreply.github.com>
2021-08-10 16:24:06 +0900

    Update linear_algebra.R

--- example/linear_algebra.R
+++ example/linear_algebra.R
@@ -163,3 +163,193 @@ cor.mat %*% eigen(cor.mat)$vector[,1]
 eigen(cor.mat)$values[1] * eigen(cor.mat)$vectors[,1]
 # 수행 시 결과가 같음.
 
+# LU분해는 하삼각행렬과 상삼각행렬의 곱으로 분해하는 것을 말하는 것.
+# 이를 미리 쪼개두면 수치적 문제에서 계산량을 줄일수 있게 해준다.
+# Matrix 라이브러리에 lu()함수를 이용하면 된다.
+# 원래 행렬에 포함된 0값들 때문에 LU분해 과정에서 발생하는 문제를 해결하기 위한 치환행렬을 계산해준다.
+
+library(Matrix)
+C <- cor.mat[1:3,1:3]
+lu.mat <- expand(lu(C))
+lower.mat <- lu.mat$L
+upper.mat <- lu.mat$U
+p.mat <- lu.mat$P
+
+#원래 행렬 C를 얻기 위해서는
+p.mat%*%lower.mat%*%upper.mat
+
+# 촐레스키 분해
+# 촐레스키 분해는 정방행렬에 적용가능한 행렬 분해법 중 하나
+# 기본 아이디어는 행렬 M을 두 개의 삼각행렬로 분해하되, 하나는 상삼각행렬 U 다른 하나는 U의 전치행렬 U^T이다
+# LU분해에서와 같이 보통 대규모의 선형일차연립방정식을 풀때 도움이 된다.
+# 상삼각행렬만 리턴한다.
+chol(cor.mat)
+t(chol(cor.mat)) %*% chol(cor.mat)
+
+# 특이값 분해는 정방행렬 뿐 아니라 직사각행렬에도 적용 가능한 행렬분해법
+# n개의 열을 갖는 행렬 M을 n개의 정보 블록들로 분해하는 방법으로 행렬 U , 행렬 V 그리고 특이값 dj들을
+# 대각원소로 갖는 대각행렬 D를 이용해 다음과 같이 표현
+# M = UDV^T 
+# 이 분해법은 특이값들을 살펴서 크기가 무시할 수 있을 정도의 작은 값에 해당되는 블록을 제거
+# 자료의 압축 또는 잡음 축소 방법에 활용된다
+
+cross.mat <- matrix(
+  c(
+    0, 0, 0, 1, 1, 0, 0, 0,
+    1, 0, 0, 1, 1, 0, 0, 0,
+    0, 0, 0, 1, 1, 0, 0, 0,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 1, 1, 0, 0, 0,
+    0, 0, 0, 1, 1, 0, 0, 0,
+    0, 0, 0, 1, 1, 0, 0, 0
+  ),byrow = TRUE, nrow=10)
+
+cross.svd.2 <- svd(cross.mat)
+round(cross.svd.2$u[,c(1,2)] %*% diag (cross.svd.2$d[c(1,2)]) %*%t(cross.svd.2$v[,c(1,2)]),0)
+
+# 라쉬 분석과 대응비교 행렬
+# 라쉬분석이라는 통계적 분석 방법
+# 척도는 1차원 잠재 특성을 나타내는 것이며, 이 잠재 특성은 원점을 가진 수직선으로 표현가능하다.
+# 개인들은 그들의 능력에 따라 이 수직선 위의 점으로 나타낼 수 있다.
+# 잠재 특성이 높은 수준인 개인은 수직선 위에 큰 값에 해당하는 점으로 나타낼 수 있다.
+# 문항의 난이도 역시 크기순으로 수직선 위에 늘어놓을 수 있다.
+# 로지스틱 분포에 기초해 특정 수준의 난이도를 가진 문항에 대해 긍정적인 대답을 할 확룰을 능력수준에 따라 계산
+
+lower.extremity.mat <- phys.func.mat[,c(2,3,4,8,9,10,13,14)]
+lower.extremity.binary <- replace(lower.extremity.mat, which(lower.extremity.mat %in% c(2:5)),0)
+
+create.paired.comparisons <- function(input.matrix) {
+  n.items <- ncol(input.matrix)
+  output.matrix <- matrix(0,nrow=n.items,ncol=n.items)
+  for(i in 1:n.items){
+    for(j in 1:n.items){
+      output.matrix[i,j] <- length(which(input.matrix[,i] - input.matrix[,j]>0))
+    }
+  }
+  return(output.matrix)
+}
+R<-create.paired.comparisons(lower.extremity.binary)
+R
+D <- t(R) / R
+diag(D) <- rep(1,8)
+D
+# 자연로그 값을 계산
+ln.D <- log(D,exp(1))
+# 각 행의 평균을 구함
+(ln.D %*% matrix(rep(1,8),nrow=8))/8
+# 라쉬 모형의 모수들을 추정하는 다양하는 방법들이 R 패키지들에 구현되어 있다.
+# eRm패키지는 라쉬 모형을 적용 및 가설검정을 위한 여러 함수를 제공
+install.packages("eRm")
+library(eRm)
+RM(lower.extremity.binary)
+
+#크론바흐 알파
+# 문항반응이론의 일부인 라쉬모형 이전에 고전검사이론이라는 방법론이 존재
+# 관측점수 = 진점수 + 오차
+# 내적일치성 그리고 일반적인 아이디어는 여러 항목 응답 사이에는 상관관계가 있어야 한다는 것이다.
+# 크론바흐 알파는 문항항목들을 둘로 쪼개는 모든 가능한 경우에 대해 구한 신뢰도의 평균값이다
+
+domains.total <- phys.func.mat %*% design.matrix
+# 공분산행렬의 대각원소 값
+tot.score.var <- diag(cov(domains.total))
+# 점수의 분산을 계산해 행벡터로 저장
+item.var <- diag(cov(phys.func.mat))
+item.var <- matrix(item.var,ncol=20)
+# 이 벡터를 계획행렬의 뒤에 곱해 도메인 별로 항목분산의 합을 계산한다.
+item.var.tot <- item.var %*% design.matrix
+# 계획행렬을 이용해 도메인별로 항목개수를 계산한다.
+n<-matrix(1,ncol=20) %*% design.matrix
+# 끝으로 이 모든 값을 알파갑을 구하는 공식에 대입
+alpha <- (n/(n-1)) * (1-(item.var.tot/tot.score.var))
+alpha
+#표준화 알파값을 구하려면 공분산 행렬 대신 상관행렬을 사용하면 된다.
+
+# DCT를 이용해 이미지를 압축하기.
+# 정방행렬에 대한 이산코사인변환을 이용한 방법
+# DCT는 위치 도메인에서 얻은 신호를 주파수 도메인 신호로 변환하는 방법중 하나
+# 변환행렬과 양자화행렬을 구하기
+
+library(png)
+picture <- readPNG('back.png')
+dim(picture)
+picture.1 <- picture[,,1]
+image(t(picture.1)[,nrow(picture.1):1],col=gray(seq(0,1,length.out=256)))
+
+picture.1.256 <- round(picture.1 * 255 -128)
+
+create.dct.matrix <- function(n) {
+  output.matrix <- matrix(0, nrow = n, ncol = n)
+  for (i in 1:n) {
+    for (j in 1:n) {
+      if (i == 1) {output.matrix[i,j] <- 1/sqrt(n)}
+      if (i > 1) {
+        output.matrix[i,j] <-
+          sqrt(2/n) * cos( (2*(j-1)+1)*(i-1)*pi / (2*n))
+      }
+    }
+  }
+  return(output.matrix)
+}
+# 양자화 행렬 정의
+quant.matrix <- matrix(
+  c(
+    16,11,10,16,24,40,51,60,
+    12,12,14,19,26,58,60,55,
+    14,13,16,24,40,57,69,56,
+    14,17,22,29,51,87,80,62,
+    18,22,37,59,68,109,103,77,
+    24,35,55,64,81,104,113,92,
+    49,64,78,87,103,121,120,101,
+    72,92,95,98,112,100,103,99
+  ),
+  byrow = TRUE,
+  nrow = 8, ncol = 8
+)
+compression.ratio <- 50
+Q <- round(quant.matrix * (100-compression.ratio)/50)
+T <- create.dct.matrix(8)
+dct.compress <- function(input.matrix) {
+  input.row <- nrow(input.matrix)
+  input.col <- ncol(input.matrix)
+  output.matrix <- matrix(0, nrow = input.row, ncol = input.col)
+  working.row <- c(1:8)
+  while (max(working.row) <= input.row) {
+    working.col <- c(1:8)
+    while (max(working.col) <= input.col) {
+      output.matrix[working.row, working.col] <-
+        (T %*% input.matrix[working.row, working.col] %*% t(T))/Q
+      working.col <- working.col + 8
+    }
+    working.row <- working.row + 8
+  }
+  return(output.matrix)
+}
+
+picture.compressed <- dct.compress(picture.1.256)
+
+image(t(picture.compressed)[,nrow(picture.compressed):1], col = gray(seq(1,0, length.out = 2)))
+
+  decompress.image <- function(input.matrix) {
+    input.row <- nrow(input.matrix)
+    input.col <- ncol(input.matrix)
+    output.matrix <- matrix(0, nrow = input.row, ncol = input.col)
+    working.row <- c(1:8)
+    while (max(working.row) <= input.row) {
+      working.col <- c(1:8)
+      while (max(working.col) <= input.col) {
+        output.matrix[working.row, working.col] <-
+          (t(T) %*% (Q * input.matrix[working.row, working.col]) %*% T)
+        working.col <- working.col + 8
+      }
+      working.row <- working.row + 8
+    }
+    return(output.matrix)
+  }
+  
+  picture.decompressed <- decompress.image(picture.compressed)
+  image(t(picture.decompressed)[,nrow(picture.decompressed):1], col = gray(seq(0,1, length.out = 256)))
+
+
