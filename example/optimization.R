c7411a6b941d1f94d9d5e6ced27c63b39049dd68
WonHeeCho <50203463+wonhecho@users.noreply.github.com>
2021-08-20 17:15:29 +0900

    Update optimization.R

--- example/optimization.R
+++ example/optimization.R
@@ -119,3 +119,57 @@ minimizer.ftn <- function(x){
   return (results)
 }
 newtonraphson(minimizer.ftn, x0=1, tol=0.000001)
+
+#넬더-미드 심플렉스 방법은 뉴튼-랩슨 방법의 대안이다
+# 미분을 사용하지 않고, 다차원에서 목적함수를 최소화하는 비선형 최적화 기법이다.
+
+optim(par = 1, fn =f)
+
+# 출력 결과를 살펴보면 최적해가 $par에 주어진 것으로 알 수 있다
+# 브렌트 방법이나, optimize()함수를 사용하라는 경고 메세지가 주어지는데 좀더 정확한 해를 구하기 위해서 이다.
+optim(1,f,method="Brent",lower=0,upper=2)
+# 결과적으로는 넬더-미드 방법과 결과가 같다.
+# EX) 로센브록함수를 최소로 하는 해를 찾는 예제
+
+rosenbrock.f <- function(x1,y1){
+  (1-x1)^2 + 100*(y1-x1^2)^2
+}
+
+# 함수의 그래프를 그려보자면
+x <- seq(-3,3,by=0.2)
+y <- seq(-2,3,by=0.2)
+z <- outer(x,y,rosenbrock.f)
+persp(x,y,z,phi=40,theta=40,col="turquoise", shade=.000001, ticktype = "detailed", cex.lab=1.5, zlab="")
+
+#로젠브록 함수의 성질 중 하나는 함수값은 언제나 양수인데, x^2,x=1을 동시에 만족하는 경우 0이 된다는 것이다.
+# optim()함수를 사용하려면 rosenbrock.f()함수의 인수가 벡터형이 되어야 하며, 첫번째 성분에 x 값을
+# 두번째 성분에 y값을 저장하도록 해야 한다.
+
+rosenbrock.f2 <- function(x){
+  (1-x[1])^2 + 100*(x[2]-x[1]^2)^2
+}
+optim(par = c(0.7,0.7),rosenbrock.f2)
+
+optim(par=c(1.5,1.5),rosenbrock.f2)
+
+# 최적화 문제에 변형된 넬더-미드 방법들을 사용해 보고 싶으면 neldermead패키키를 확인
+# optim()함수로 사용할 수 있는 method = {"Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN", "Brent"}
+# 모의담금질이라는 기법을 이용하고 싶으면 SANN
+
+set.seed(267)
+optim(par=c(0.7,0.7),rosenbrock.f2, method="SANN")
+
+# 준뉴튼이라는 방법을 이용한 예제로는
+h <- function(x){
+  (x[1]-2)^2 + (x[2]-1)^2
+}
+
+x1 <- seq(-4,4,by=0.5)
+x2 <- seq(-4,4,by=0.5)
+mat <- as.matrix(expand.grid(x1,x2))
+colnames(mat) <- c("x1","x2")
+z <- apply(mat,1,h)
+df <- data.frame(mat,z)
+library(lattice)
+wireframe(z~x1*x2, data=df, shade = TRUE, scales = list(arrows=FALSE), screen = list(z=-35,x=-50))
+
